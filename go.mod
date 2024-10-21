17960a0151d9e5d21eea6f68b7dc546d0b1a1b52
Kohei Matsuo <koheiihphone@icloud.com>
2024-10-21 23:21:04 +0900

    UsersのCRUD操作

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/acmk189/golang_udemy_todo_app
 go 1.22.5
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,14 +8,38 @@ import (
 
 func main() {
 	/*
-		fmt.Println(config.Config.Port)
-		fmt.Println(config.Config.SQLDriver)
-		fmt.Println(config.Config.DbName)
-		fmt.Println(config.Config.LogFile)
+			fmt.Println(config.Config.Port)
+			fmt.Println(config.Config.SQLDriver)
+			fmt.Println(config.Config.DbName)
+			fmt.Println(config.Config.LogFile)
 
-		log.Println("test")
+			log.Println("test")
+
+		fmt.Println(models.Db)
+
+			u := &models.User{}
+			u.Name = "test"
+			u.Email = "test@example.com"
+			u.Password = "testtest"
+			fmt.Println(u)
+
+			u.CreateUser()
 	*/
 
-	fmt.Println(models.Db)
+	u, _ := models.GetUser(1)
+
+	fmt.Println(u)
+
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+
+	u, _ = models.GetUser(1)
+	u.UpdateUser()
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 
 }
