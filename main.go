e80fc29e385c3929ad4b624f3245a2091f3f9898
Kohei Matsuo <koheiihphone@icloud.com>
2024-10-25 14:32:51 +0900

    add: server機能追加

--- main.go
+++ main.go
@@ -1,71 +1,16 @@
 package main
 
-import "github.com/acmk189/golang_udemy_todo_app/app/models"
+import (
+	"fmt"
+
+	"github.com/acmk189/golang_udemy_todo_app/app/controllers"
+	"github.com/acmk189/golang_udemy_todo_app/app/models"
+)
 
 func main() {
-	/*
-					fmt.Println(config.Config.Port)
-					fmt.Println(config.Config.SQLDriver)
-					fmt.Println(config.Config.DbName)
-					fmt.Println(config.Config.LogFile)
 
-					log.Println("test")
+	fmt.Println(models.Db)
 
-				fmt.Println(models.Db)
-
-			u := &models.User{}
-			u.Name = "test"
-			u.Email = "test@example.com"
-			u.Password = "testtest"
-			fmt.Println(u)
-
-			u.CreateUser()
-
-				u, _ := models.GetUser(1)
-
-				fmt.Println(u)
-
-				u.Name = "Test2"
-				u.Email = "test2@example.com"
-				u.UpdateUser()
-
-				u, _ = models.GetUser(1)
-				u.UpdateUser()
-				fmt.Println(u)
-
-				u.DeleteUser()
-				u, _ = models.GetUser(1)
-				fmt.Println(u)
-
-			user, _ := models.GetUser(2)
-			user.CreateTodo("First Todo")
-
-				t, _ := models.GetTodos
-				fmt.Println(t)
-
-			user, _ := models.GetUser(1)
-
-				user.CreateTodo("Final Todo")
-
-					todos, _ := models.GetTodos()
-					for _, v := range todos {
-						fmt.Println(v)
-					}
-
-
-		todos, _ := user.GetTodosByUser()
-		for _, v := range todos {
-			fmt.Println(v)
-		}
-	*/
-
-	/*
-		t, _ := models.GetTodo(1)
-		t.Content = "Update Todo"
-		t.UpdateTodo()
-	*/
-
-	t, _ := models.GetTodo(1)
-	t.DeleteTodo()
+	controllers.StartMainServer()
 
 }
